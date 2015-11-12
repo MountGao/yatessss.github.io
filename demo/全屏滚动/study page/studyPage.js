8722072cccdf0cb269adff15deae53a84b2a4df4
yatessss <897969073@qq.com>
2015-11-12 23:20:43 +0800

    为banner添加了一个动画效果
    add animate for banner

--- demo/全屏滚动/study page/studyPage.js
+++ demo/全屏滚动/study page/studyPage.js
@@ -1,28 +1,35 @@
-/*$(document).ready(function(){
-	$('#fullpage').fullpage({
-		navigation : true,
-		navigationPosition : 'right',
-		navigationColor : '#fff',
-		paddingTop : '40px' , //页面距离顶部的距离
-		fixedElements : '#header-area'//设置元素固定不动 配合jquery选择器使用
-	})
+$(document).ready(function(){
+	naviBtn();
+	animate();
 
-});*/
-
-$('#navi-button').click(function(){
-	var width = $(document).width();
-	if ($('#navi-menu').offset().left > width ) { 
-		console.log($(document).width());
-		$('#navi-menu').animate({
-		right:'80px',
-		});
-		return;
-	};
-	if($('#navi-menu').offset().left < width ){
-		console.log($(document).width());
-		$('#navi-menu').animate({
-		right:'-500px',
-		});
-		return;
-	}
 });
+
+var naviBtn =function(){
+	$('#navi-button').click(function(){
+		var width = $(document).width();
+		if ($('#navi-menu').offset().left > width ) { 
+			console.log($(document).width());
+			$('#navi-menu').animate({
+			right:'80px',
+			});
+			return;
+		};
+		if($('#navi-menu').offset().left < width ){
+			console.log($(document).width());
+			$('#navi-menu').animate({
+			right:'-500px',
+			});
+			return;
+		}
+	});
+}
+
+function animate(){
+	$('#cover-layer').animate({opacity: '0.8'},2000,
+		function(){$('#banner-content h1').animate({opacity:'1'},1500,
+			function(){$('#banner-content h2').animate({opacity:'1'},1500,
+				function(){$('#banner-content a').animate({opacity:'1'},1500);}
+			);}
+		);}
+	);
+}
