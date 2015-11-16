a1a3a9764f2a81e6e4a77ccebad466903f8b709c
yatessss <897969073@qq.com>
2015-11-16 23:54:22 +0800

    添加新的页面
    add a new page

---
+++ demo/new page/studyPage.js
@@ -0,0 +1,42 @@
+/*$(document).ready(function(){
+	naviBtn();
+	animate();
+	onmouse()
+
+});
+
+var naviBtn =function(){
+	$('#navi-button').click(function(){
+		var width = $(document).width();
+		if ($('#navi-menu').offset().left > width ) { 
+			console.log($(document).width());
+			$('#navi-menu').animate({
+			right:'80px',
+			});
+			return;
+		};
+		if($('#navi-menu').offset().left < width ){
+			console.log($(document).width());
+			$('#navi-menu').animate({
+			right:'-500px',
+			});
+			return;
+		}
+	});
+}
+
+function animate(){
+	$('#cover-layer').animate({opacity: '0.8'},2000,
+		function(){$('#banner-content h1').animate({opacity:'1'},1500,
+			function(){$('#banner-content h2').animate({opacity:'1'},1500,
+				function(){$('#banner-content a').animate({opacity:'1'},1500);}
+			);}
+		);}
+	);
+}*/
+
+/*function onmouse(){
+	$('#introduce-content div.part1-pic').mouseover(function(){
+		$('#introduce-content div.part1-pic').animate({backgroundSize:'100%'},5000);
+	});
+}*/
